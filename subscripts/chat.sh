be545287cdfd3f0e82af0d8f1f9552b107122810
Olsson <78812304+olssonik@users.noreply.github.com>
2021-11-25 16:25:09 +0000

    Create chat.sh

---
+++ subscripts/chat.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+clear;
+
+# wybierz useername
+
+echo "What do u want ur username to be?"
+read username
+
+# wybierz chat w ktorym chesz rozmawiac
+        echo " u need to use chat 1 anyway rn"
+
+        #rozmowa
+        x=1
+until [ $x -gt 10 ]
+do
+        clear
+        cat cache/chats/chat
+
+        echo "------------------------------------ (CTRL + C) to quit "
+        read newlinechat
+        echo "<$username> $newlinechat" >> ~/discord-2.0/cache/chats/chat
+done
